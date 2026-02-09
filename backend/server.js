39b985f310874473ab6652221d4801c22226001c
Preetam Naik <preetamnaik75@gmail.com>
2026-02-09 15:20:22 +0530

    Initial Commit

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import cors from "cors"; 
+import cors from "cors";
 
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
