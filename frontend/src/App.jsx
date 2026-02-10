d3e5b0881b7ffe646c4839f8a8a54b96a25e2d6b
Preetam Naik <preetamnaik75@gmail.com>
2026-02-10 14:29:23 +0530

    Initial Commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -62,10 +62,7 @@ function App() {
 					/>
 					<Route path='/category/:category' element={<CategoryPage />} />
 					<Route path='/cart' element={user ? <CartPage /> : <Navigate to='/login' />} />
-					<Route
-						path='/purchase-success'
-						element={user ? <PurchaseSuccessPage /> : <Navigate to='/login' />}
-					/>
+					<Route path='/purchase-success' element={<PurchaseSuccessPage />} />
 					<Route path='/purchase-cancel' element={user ? <PurchaseCancelPage /> : <Navigate to='/login' />} />
 				</Routes>
 			</div>
